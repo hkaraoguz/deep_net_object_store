9d55765c960e19b08c1e02d851c6ddeea8afaeab
Hakan <hkaraoguz@yahoo.com.tr>
2016-11-29 10:55:33 +0100

    Changes made for listening quasimodo done msg and then start processing the sweep

--- src/deep_net_object_store_node.cpp
+++ src/deep_net_object_store_node.cpp
@@ -3,7 +3,7 @@
 #include <semantic_map/RoomObservation.h>
 #include "deep_net_object_store/util.h"
 
-
+#include <std_msgs/String.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <ros/subscriber.h>
 #include <soma_msgs/SOMAObject.h>
@@ -27,8 +27,110 @@ MongodbInterface* m_MongodbInterface;
 
 ros::ServiceClient soma_insert_service;
 
+std::string metaroomxmlpath="";
 
-void processMetaRoom(const semantic_map::RoomObservation& observation)
+void quasimodoDoneCallback(const std_msgs::String::ConstPtr& msg)
+{
+    metaroomxmlpath = msg->data;
+
+    if(metaroomxmlpath != "")
+    {
+
+        ROS_INFO("Processing Metaroom for deep_net objects");
+
+        deep_object_detection::DetectObjects detect_objects;
+
+
+        RoomObservation roomobservation = readRGBImagesfromRoomSweep(metaroomxmlpath,sweepCenter);
+
+        ROS_INFO("Observation path %s",metaroomxmlpath.data());
+
+        ROS_INFO("Images and clouds size: %lu",roomobservation.rosimagesclouds.size());
+
+        if(roomobservation.rosimagesclouds.size() > 0)
+        {
+
+            /************Transform images into ros images***********************/
+            std::vector<sensor_msgs::Image > rosimages;
+
+            std::vector<Cloud> clouds;
+
+            for(int i = 0; i < roomobservation.rosimagesclouds.size(); i++)
+            {
+
+                rosimages.push_back(roomobservation.rosimagesclouds[i].first);
+                clouds.push_back(roomobservation.rosimagesclouds[i].second);
+
+            }
+            /*******************************************************************/
+
+
+            detect_objects.request.images = rosimages;
+            detect_objects.request.confidence_threshold = 0.8;
+
+
+
+            // If we can call the service and get a response
+            if(object_detection_service.call(detect_objects))
+            {
+                ROS_INFO("Service call successfull!");
+                ROS_INFO("Num objects %lu",detect_objects.response.objects.size());
+                if(detect_objects.response.objects.size() > 0)
+                {
+
+
+                    // We refine the detected objects based on distance to not to include same object multiple times
+                    std::vector< std::pair<deep_object_detection::Object,Cloud> > refinedObjectsCloudsPair = refineObjects(detect_objects.response.objects,clouds,rosimages[0].width,std::vector<std::string>(),sweepCenter);
+
+                    for(int j = 0; j < refinedObjectsCloudsPair.size(); j++)
+                    {
+
+                        std::pair<deep_object_detection::Object,Cloud> apair = refinedObjectsCloudsPair[j];
+
+                        RoomObject aroomobject;
+
+                        aroomobject.object = apair.first;
+                        aroomobject.cloud = apair.second;
+
+                        // aroomobject.image = roomobservation.rosimagesclouds[apair.first.imageID];
+
+                        cv_bridge::CvImagePtr ptr = cv_bridge::toCvCopy(roomobservation.rosimagesclouds[apair.first.imageID].first,"bgr8");
+
+
+                        cv::rectangle(ptr->image,cv::Point(refinedObjectsCloudsPair[j].first.x,refinedObjectsCloudsPair[j].first.y),
+                                      cv::Point(refinedObjectsCloudsPair[j].first.x+refinedObjectsCloudsPair[j].first.width, refinedObjectsCloudsPair[j].first.y + refinedObjectsCloudsPair[j].first.height),cv::Scalar(255,255,0));
+
+
+                        cv::putText(ptr->image,refinedObjectsCloudsPair[j].first.label.data(),cv::Point(refinedObjectsCloudsPair[j].first.x+refinedObjectsCloudsPair[j].first.width*0.25,refinedObjectsCloudsPair[j].first.y+refinedObjectsCloudsPair[j].first.height*0.5),0,2.0,cv::Scalar(255,255,0));
+
+
+                        ptr->toImageMsg(aroomobject.rosimage);
+
+                        roomobservation.roomobjects.push_back(aroomobject);
+
+
+
+                    }
+                    if(roomobservation.roomobjects.size() > 0)
+                        m_MongodbInterface->logSOMAObjectsToDBCallService(soma_insert_service,roomobservation.room,roomobservation);
+
+                }
+            }
+
+        }
+    }
+    else
+    {
+        ROS_INFO("Cannot process metaroom since the path is not correct");
+
+    }
+
+    metaroomxmlpath = "";
+}
+
+
+
+void getMetaRoomPath(const semantic_map::RoomObservation& observation)
 {
 
     struct stat info;
@@ -38,90 +140,15 @@ void processMetaRoom(const semantic_map::RoomObservation& observation)
         return ;
     }
 
-    ROS_INFO("Processing Metaroom for deep_net objects");
+    metaroomxmlpath = observation.xml_file_name;
 
-    deep_object_detection::DetectObjects detect_objects;
-
-
-    RoomObservation roomobservation = readRGBImagesfromRoomSweep(observation.xml_file_name,sweepCenter);
-
-     ROS_INFO("Observation path %s",observation.xml_file_name.data());
-
-    ROS_INFO("Images and clouds size: %lu",roomobservation.rosimagesclouds.size());
-
-    if(roomobservation.rosimagesclouds.size() > 0)
-    {
-
-        /************Transform images into ros images***********************/
-        std::vector<sensor_msgs::Image > rosimages;
-
-        std::vector<Cloud> clouds;
-
-        for(int i = 0; i < roomobservation.rosimagesclouds.size(); i++)
-        {
-
-            rosimages.push_back(roomobservation.rosimagesclouds[i].first);
-            clouds.push_back(roomobservation.rosimagesclouds[i].second);
-
-        }
-        /*******************************************************************/
-
-
-        detect_objects.request.images = rosimages;
-        detect_objects.request.confidence_threshold = 0.8;
+    ROS_INFO("Received sweep path");
 
 
 
-        // If we can call the service and get a response
-        if(object_detection_service.call(detect_objects))
-        {
-            ROS_INFO("Service call successfull!");
-            ROS_INFO("Num objects %lu",detect_objects.response.objects.size());
-            if(detect_objects.response.objects.size() > 0)
-            {
 
 
-                // We refine the detected objects based on distance to not to include same object multiple times
-                std::vector< std::pair<deep_object_detection::Object,Cloud> > refinedObjectsCloudsPair = refineObjects(detect_objects.response.objects,clouds,rosimages[0].width,std::vector<std::string>(),sweepCenter);
-
-                for(int j = 0; j < refinedObjectsCloudsPair.size(); j++)
-                {
-
-                    std::pair<deep_object_detection::Object,Cloud> apair = refinedObjectsCloudsPair[j];
-
-                    RoomObject aroomobject;
-
-                    aroomobject.object = apair.first;
-                    aroomobject.cloud = apair.second;
-
-                    // aroomobject.image = roomobservation.rosimagesclouds[apair.first.imageID];
-
-                    cv_bridge::CvImagePtr ptr = cv_bridge::toCvCopy(roomobservation.rosimagesclouds[apair.first.imageID].first,"bgr8");
-
-
-                    cv::rectangle(ptr->image,cv::Point(refinedObjectsCloudsPair[j].first.x,refinedObjectsCloudsPair[j].first.y),
-                                  cv::Point(refinedObjectsCloudsPair[j].first.x+refinedObjectsCloudsPair[j].first.width, refinedObjectsCloudsPair[j].first.y + refinedObjectsCloudsPair[j].first.height),cv::Scalar(255,255,0));
-
-
-                    cv::putText(ptr->image,refinedObjectsCloudsPair[j].first.label.data(),cv::Point(refinedObjectsCloudsPair[j].first.x+refinedObjectsCloudsPair[j].first.width*0.25,refinedObjectsCloudsPair[j].first.y+refinedObjectsCloudsPair[j].first.height*0.5),0,2.0,cv::Scalar(255,255,0));
-
-
-                    ptr->toImageMsg(aroomobject.rosimage);
-
-                    roomobservation.roomobjects.push_back(aroomobject);
-
-
-
-                }
-                if(roomobservation.roomobjects.size() > 0)
-                    m_MongodbInterface->logSOMAObjectsToDBCallService(soma_insert_service,roomobservation.room,roomobservation);
-
-            }
-        }
-
-    }
-
-   /* if(!ros::ok())
+    /* if(!ros::ok())
     {
         ROS_INFO("Breaking loop...");
         break;
@@ -161,12 +188,15 @@ int main(int argc, char** argv)
 
 
 
-    ros::Subscriber sub = n.subscribe("/local_metric_map/room_observations", 1, processMetaRoom);
+    ros::Subscriber sub = n.subscribe("/local_metric_map/room_observations", 1, getMetaRoomPath);
 
 
     object_detection_service = n.serviceClient<deep_object_detection::DetectObjects>("/deep_object_detection/detect_objects");
 
     soma_insert_service = n.serviceClient<soma_manager::SOMAInsertObjs>("soma/insert_objects");
+
+    ros::Subscriber sub2 = n.subscribe("/vocabulary_done", 1, quasimodoDoneCallback);
+
 
 
 
